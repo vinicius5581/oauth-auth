50de774d80710fcb6d5910092b0e0615571cb191
vinicius5581 <viniciussantana@Viniciuss-MacBook-Pro.local>
2018-08-17 12:15:58 -0700

    Adding session cookie

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const authRoutes = require("./routes/auth-routes");
 const passportSetup = require("./config/passport-setup");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -23,6 +25,17 @@ mongoose.connect(
 
 // set up view engine
 app.set("view engine", "ejs");
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+  })
+);
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set up routes
 app.use("/auth", authRoutes);
