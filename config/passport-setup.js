87d9771a39e2d8d1eb368d92ef90e58a78defe84
vinicius5581 <viniciussantana@Viniciuss-MacBook-Pro.local>
2018-08-17 12:00:56 -0700

    adding serialize and deserialize user on passport setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys");
 const User = require("../models/user-model");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,14 +23,13 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       // passport callback function
-      // console.log("passport callback function fired");
-      // console.log(profile);
       User.findOne({
         googleId: profile.id
       }).then(currentUser => {
         if (currentUser) {
           // already have the user
           console.log("User is: " + currentUser);
+          done(null, currentUser);
         } else {
           // if not, create user in our db
           const user = new User({
@@ -30,6 +39,7 @@ passport.use(
 
           user.save().then(newUser => {
             console.log("New user created: " + newUser);
+            done(null, newUser);
           });
         }
       });
